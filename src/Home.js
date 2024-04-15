1726e107ef7f9f926d94b144030dbafda45905af
nourddine faouzi <nourddine.faouzi16@gmail.com>
2024-04-15 17:20:15 +0100

    footer added

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logout from './Logout';
+import Footer from './Footer';
 
 function Home() {
     return (
       <>
         <Logout />
         <h1>This is the Home</h1>
+        <Footer />
       </>
 
     );
